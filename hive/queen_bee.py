97d7ec1215ced715e1076ec6a9ac41de35eb1765
rabidlego25 <geneatabb@outlook.com>
2024-05-01 17:12:24 -0700

    migrated to jupyter

--- hive/queen_bee.py
+++
@@ -1,7 +0,0 @@
-from input_handler import get_user_input
-from hamiltonian_generator import define_hamiltonian
-
-d, f, d_a, ibc, num_p, is_i  =  get_user_input()
-H = define_hamiltonian(f,d_a,num_p)
-
-print(H)
